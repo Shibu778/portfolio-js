ede82f8c1a75c7280acade33410007763ba38c24
Shibu Meher <56470750+Shibu778@users.noreply.github.com>
2024-04-26 14:10:35 +0000

    updated site

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@
 
 import React from "react";
 
+import Education from "./Components/Education";
 import About from "./Components/About";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
@@ -46,6 +47,7 @@ const App = () => {
       <Home name={siteProps.name} title={siteProps.title} />
       <About />
       <Portfolio />
+      <Education />
       <Footer {...siteProps} primaryColor={primaryColor} secondaryColor={secondaryColor} />
     </div>
   );
